f0a91bb18fbeb4b2825b4a7a78818450e262b6ae
yeonjun-git <lyj09029@gmail.com>
2024-05-30 21:46:13 +0900

    sampling 1.0

--- module/sqlite.py
+++ module/sqlite.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 from fileoperator import BaseOperator
 
-class sqlite:
+class SqliteSQL:
     
     def __init__(self, data_path):
         self.data_path = data_path
@@ -18,16 +18,4 @@ class sqlite:
         return data
     
 if __name__ == '__main__':
-       
-    data_path = BaseOperator.set_path(
-        work_directory='finance',
-        target_path='/lotto_data/lotto_data.db'
-    )    
-        
-    db = sqlite(data_path)
-    
-    query = "select * from tb_lotto_list"
-    data = db.execute(query)
-    print(data)
-    
-        
+       pass
